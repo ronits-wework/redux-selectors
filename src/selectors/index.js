6a32ca2d21fa64056297574c7a82ce710689d206
Ronit Steinberg <ronit.steinberg@wework.com>
2018-05-19 18:08:41 +0300

    use reselect

--- src/selectors/index.js
+++ src/selectors/index.js
@@ -1,22 +1,24 @@
 import {VisibilityFilters} from '../actions'
+import {createSelector} from 'reselect'
 
 export const getVisibilityFilter = (state) => state.visibilityFilter;
 export const getTodos = (state) => state.todos;
 
-export const getVisibleTodos = (state) => {
-  const filter = getVisibilityFilter(state);
-  const todos = getTodos(state);
-  switch (filter) {
-    case VisibilityFilters.SHOW_ALL:
-      return todos;
-    case VisibilityFilters.SHOW_COMPLETED:
-      return todos.filter(t => t.completed);
-    case VisibilityFilters.SHOW_ACTIVE:
-      return todos.filter(t => !t.completed);
-    default:
-      throw new Error('Unknown filter: ' + filter);
+export const getVisibleTodos = createSelector(
+  [getVisibilityFilter, getTodos],
+  (filter, todos) => {
+    switch (filter) {
+      case VisibilityFilters.SHOW_ALL:
+        return todos;
+      case VisibilityFilters.SHOW_COMPLETED:
+        return todos.filter(t => t.completed);
+      case VisibilityFilters.SHOW_ACTIVE:
+        return todos.filter(t => !t.completed);
+      default:
+        throw new Error('Unknown filter: ' + filter);
+    }
   }
-};
+);
 
 const getTotalEstimatesForTodos = (todos) => {
   return todos.reduce(
@@ -26,12 +28,16 @@ const getTotalEstimatesForTodos = (todos) => {
     , 0);
 };
 
-export const getTotalEstimates = (state) => {
-  const todos = getTodos(state);
-  return getTotalEstimatesForTodos(todos);
-};
+export const getTotalEstimates = createSelector(
+  [getTodos],
+  (todos) => {
+    return getTotalEstimatesForTodos(todos);
+  }
+);
 
-export const getTotalVisibleEstimates = (state) => {
-  const todos = getVisibleTodos(state);
-  return getTotalEstimatesForTodos(todos);
-};
+export const getTotalVisibleEstimates = createSelector(
+  [getVisibleTodos],
+  (todos) => {
+    return getTotalEstimatesForTodos(todos);
+  }
+);
