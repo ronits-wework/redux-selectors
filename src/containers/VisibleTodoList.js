582a04eeaa4d59f5be8f542e1b2c30ebe62d7b73
Ronit Steinberg <ronit.steinberg@wework.com>
2018-05-25 15:42:28 +0300

    remove mapDispatchToProps

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,7 +1,5 @@
 import { connect } from 'react-redux'
-import { toggleTodo } from '../actions'
 import TodoList from '../components/TodoList'
-import { VisibilityFilters } from '../actions'
 
 const mapStateToProps = state => ({
   todos: state.todos
